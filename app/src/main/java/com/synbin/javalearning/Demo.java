fe300b0db9e16728174f5432fac14140819b6c07
John Spink <john.spink@gmail.com>
2017-11-06 15:03:01 +0000

    Video 2-04 Variables and Constants

--- app/src/main/java/com/synbin/javalearning/Demo.java
+++ app/src/main/java/com/synbin/javalearning/Demo.java
@@ -4,7 +4,25 @@ package com.synbin.javalearning;
 
 public class Demo {
 
+	// variables - mutable (can be changed)
+	static int lives = 3;
+	static String playerName = "John";
+	static boolean gameOver;                // false by default
+
+	static final int LEVEL_COUNT = 4;		// immutable (constant), cannot be changed
+
 	public static void main(String[] args) {
-		System.out.println("Hello, Java World!");
+
+		System.out.println("player " + playerName + " has " + lives + " lives left");
+
+		lives += 2;
+		playerName += " Spink";
+		System.out.println("player " + playerName + " has " + lives + " lives left");
+		System.out.println("gameOver = " + gameOver);
+
+		System.out.println("player " + playerName + " needs to complete " + LEVEL_COUNT + " levels");
+
+		gameOver = true;
+		System.out.println("gameOver = " + gameOver);
 	}
 }
