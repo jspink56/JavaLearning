4cf444f2f4de385a9ac62a0ca2285f0d289d8f31
John Spink <john.spink@gmail.com>
2017-11-06 14:41:00 +0000

    Android LibGDX Game Development Masterclass Section 2 - Java & Design Patterns Introduction Initial commit

---
+++ app/src/main/java/com/synbin/javalearning/Demo.java
@@ -0,0 +1,10 @@
+package com.synbin.javalearning;
+// Created by John on 06-Nov-17
+
+
+public class Demo {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, Java World!");
+	}
+}
